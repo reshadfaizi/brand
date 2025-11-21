290c97f96061e870964750bf850696913c65d754
reshadfaizi <faizireshad7@gmail.com>
2025-11-21 16:32:11 +0500

    madia

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,4 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{html,js}"],
